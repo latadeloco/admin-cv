340ac59e23a190afbe3457fc13877a75ebe9360c
Jesús Robles Sánchez <jesusroblessanchez@gmail.com>
2020-11-03 12:58:53 +0100

    Se quita app.global.js tanto de git como de angular.json y se introducen los métodos click en el header para que quede todo en header.component.ts

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 import { Opcion, OpcionesMenuService } from 'src/app/services/opciones-menu.service';
-import { AppComponent } from "../../../app.component";
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
     selector: 'app-header',
@@ -16,8 +17,14 @@ export class HeaderComponent implements OnInit {
     /**
      * Constructor del componente
      * @param _opcionesMenuService Parámetro necesario para recoger las opciones del menú
+     * @param usuarioService Parámetro para ver si el usuario tiene el token activo en el navegador
+     * @param router Parámetro para redireccionar en caso de token KO
      */
-    constructor( private _opcionesMenuService : OpcionesMenuService ) {
+    constructor( 
+        private _opcionesMenuService : OpcionesMenuService,
+        private usuarioService : UsuarioService,
+        private router: Router
+         ) {
         this.hashPathDefault = window.location.hash;
     }
     urlImg: string;
@@ -41,5 +48,29 @@ export class HeaderComponent implements OnInit {
             menu.children[i].children[0].classList.remove("bg-light");
         }
         opcion.classList.add('bg-light');
+    }
+
+    /**
+     * Cerrar sesión, se pregunta por el token y en caso de que lo tenga lo elimina y lo redirecciona a iniciar-sesión.
+     */
+    logOut() {
+        if (this.usuarioService.getUserToken()) {
+            localStorage.removeItem('user-token');
+        }
+
+        this.router.navigateByUrl('/iniciar-sesion');
+    }
+
+    /**
+     * Hacer que el navegador sea fullScreen (pantalla completa)
+     */
+    fullScreen() {
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+        } else {
+            if (document.exitFullscreen) {
+            document.exitFullscreen(); 
+            }
+        }
     }
 }
